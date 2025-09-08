e098defbf52ed72f1ed3f12212078b61ea880ff2
MohamedAymanGamal <ma2030g@hotmail.com>
2025-09-08 22:36:09 +0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { Trello } from 'lucide-react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div>
+            <Trello/>
+            <span>jira clone</span>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
